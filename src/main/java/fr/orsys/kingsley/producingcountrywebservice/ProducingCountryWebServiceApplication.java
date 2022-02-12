66b2b353382f74e86df09dd8cdca66b33245d3fe
Administrator <Administrator@10.0.2.60>
2022-02-12 03:38:10 +0100

    First commit on country soap service

---
+++ src/main/java/fr/orsys/kingsley/producingcountrywebservice/ProducingCountryWebServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.orsys.kingsley.producingcountrywebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducingCountryWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducingCountryWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/orsys/kingsley/producingcountrywebservice/ProducingCountryWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orsys.kingsley.producingcountrywebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducingCountryWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
